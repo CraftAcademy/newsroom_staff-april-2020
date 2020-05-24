fda054f875cd414357a565003865bfe34a784bc6
designerofthing <designerofthings@gmail.com>
2020-05-24 11:07:22 +0200

    adds feature test for journalist login

---
+++ cypress/integration/journalistCanLogin.feature.js
@@ -0,0 +1,25 @@
+describe('Journalist authenticates', () => {
+  beforeEach(() => {
+    cy.visit('/');
+  });
+
+  it('successfully with valid credentials', () => {
+    cy.get('#login').click();
+    cy.get('#login-form').within(() => {
+      cy.get('#email').type('user@mail.com');
+      cy.get('#password').type('password');
+      cy.get('button').contains('Submit').click();
+    });
+    cy.get('#message').should('contain', 'Welcome to work user@mail.com');
+  });
+
+  it("unsuccessfully with invalid credentials", () => {
+    cy.get("#login").click();
+    cy.get("#login-form").within(() => {
+      cy.get("#email").type("user@mail.com");
+      cy.get("#password").type("wrongpassword");
+      cy.get('button').contains('Submit').click()
+    });
+    cy.get("#message").should("contain", "Invalid login credentials, please try again.");
+  });
+});
