c7ad3a41ffca0bf9aa87a74347ae7f4e548312f0
Erik Björn <erik.g.bjoern@gmail.com>
2020-05-24 20:36:17 +0200

    adding assertion for logged in staff, adding redirection to test

--- cypress/integration/journalistCanLogin.feature.js
+++ cypress/integration/journalistCanLogin.feature.js
@@ -4,33 +4,46 @@ describe('Journalist authenticates', () => {
     cy.visit('/');
   });
 
-  it('successfully with valid credentials', () => {
-    cy.route({
-      method: "POST",
-      url: "http://localhost:3000/api/auth/*",
-      response: "fixture:successful_login.json",
-      headers: {
-        uid:"user@mail.com"
-      }
-    })
-    cy.route({
-      method: "GET",
-      url: "http://localhost:3000/api/auth/*",
-      response: "fixture:successful_login.json",
-      headers: {
-        uid:"user@mail.com"
-      }
-    })
-    cy.get('#login-form').within(() => {
-      cy.get('#email').type('user@mail.com');
-      cy.get('#password').type('password');
-      cy.get('Button').contains('Submit').click();
-    });
-    cy.get('#header').should('contain', 'Log out user@mail.com');
-  });
+  it('does not see the article form', () => {
+    cy.get('button#post').should('not.exist')
+  })
 
-  it("unsuccessfully with invalid credentials", () => {
-    cy.route({
+  describe('successfully', () => {
+    beforeEach(() => {
+      cy.route({
+        method: "POST",
+        url: "http://localhost:3000/api/auth/*",
+        response: "fixture:successful_login.json",
+        headers: {
+          uid:"user@mail.com"
+        }
+      })
+      cy.route({
+        method: "GET",
+        url: "http://localhost:3000/api/auth/*",
+        response: "fixture:successful_login.json",
+        headers: {
+          uid:"user@mail.com"
+        }
+      })
+      cy.get('#login-form').within(() => {
+        cy.get('#email').type('user@mail.com');
+        cy.get('#password').type('password');
+        cy.get('Button').contains('Submit').click();
+      });
+    })
+
+    it('with valid credentials', () => {
+      cy.get('.header').should('contain', 'Log out user@mail.com');
+    });
+
+    it('and is redirected to Write page', () => {
+      cy.get('button#post').should('be.visible')
+    })
+  })
+    
+    it("unsuccessfully with invalid credentials", () => {
+      cy.route({
       method: "POST",
       url: "http://localhost:3000/api/auth/sign_in",
       response: "fixture:unsuccessful_login.json",
--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,15 @@ import Header from './components/Header'
 
 function App() {
   const [message, setMessage] = useState("")
+  const [uid, setUid] = useState("")
+  const [authenticated, setAuthenticated] = useState(false)
 
   const logIn = async (e) => {
     try {
       const response = await auth.signIn(e.target.email.value, e.target.password.value)
+      debugger
+      setUid(response.data.uid)
+      setAuthenticated(true)
     } catch(error) {
       debugger
       setMessage(error.response.data.errors[0])
@@ -18,7 +23,7 @@ function App() {
 
   return (
     <div className="App">
-      <Header/>
+      <Header uid={uid} authenticated={authenticated}/>
       <LoginForm logIn={logIn} errorMessage={message}/>
       <CreateArticle/>
     </div>
